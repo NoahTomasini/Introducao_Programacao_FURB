3d4ee99bb588038e991db00a587ad4631188c358
Noah <tomasinin2007@gmail.com>
2026-03-19 19:52:04 -0300

    Unidade 4 adicionada

---
+++ Unidade_4/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        Float media = 5F;
+        if(media >= 6) {
+            System.out.println("Aprovado");
+        }
+        else {
+            System.out.println("Reprovado");
+        }
+    }
+}
+// para and utilizar &&; para ou utilizar || (pipe/barra vertical)
